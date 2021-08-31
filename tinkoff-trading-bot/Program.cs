fd1d7d1955515179ea839c12d782f8080f668ff0
n.skorokhod <mtwain.mark@gmail.com>
2021-08-31 21:05:54 +0300

    add Sandbox test

---
+++ tinkoff-trading-bot/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace tinkoff_trading_bot
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var token = (await File.ReadAllTextAsync("token.txt")).Trim();
+            await using var bot = new SandboxBot(token);
+            await bot.StartAsync();
+        }
+    }
+}
